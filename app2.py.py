2505229a79783b070306159372cb7f9f701460b0
sahibzadakashif <kashifsahibzada88@gmail.com>
2024-11-18 13:01:17 +0500

    app2.py.py

--- app2.py.py
+++ app2.py.py
@@ -76,8 +76,7 @@ def desc_calc(smiles_input):
 def filedownload(df):
  csv = df.to_csv(index=False)
  b64 = base64.b64encode(csv.encode()).decode() # strings <-> bytes conversions
- href = f'<a href="data:file/csv;base64,{b64}" download="prediction.csv">Download 
-Predictions</a>'
+ href = f'<a href="data:file/csv;base64,{b64}" download="prediction.csv">Download Predictions</a>'
 
  return href
 def build_model(input_data, smiles_list):
